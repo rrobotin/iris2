ec93da8ddac1e1595f4d5f73376bdf06268e6fff
Roxana Robotin <rrobotin@mozilla.com>
2019-03-26 14:00:23 +0200

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo -e "PWD #####\n"
 pwd
-cd ~/mozilla/iris
+cd ~/rrobotin/iris2
 echo -e "PWD #####\n"
 pwd
 export DISPLAY=:99.0
 Xvfb :99 -screen 0 1920x1080x24+32 +extension GLX +extension RANDR > /dev/null 2>&1 &
 sleep 3
 echo -e " start CI tests ##### \n"
-sudo pipenv run iris -j -n -d ci_tests -i DEBUG
+sudo pipenv run iris firefox ci_tests/test_fake_keyboard_input.py -vk -i DEBUG
 echo -e " FINISH RUNNING TESTS !!!! ##### \n"
